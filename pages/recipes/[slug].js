c376fbcefa950b665f6a6f128278f4c2167e0602
mdtauqeeralam21 <mdta2108@gmail.com>
2023-07-07 12:00:41 +0530

    nextjs demo app

---
+++ pages/recipes/[slug].js
@@ -0,0 +1,25 @@
+
+export const getStaticPaths= async()=>{
+
+
+    return{
+        paths:
+    }
+}
+
+export const getStaticProps= async()=>{
+
+    return{
+        props:{
+            
+        }
+    }
+}
+
+export default function RecipeDetails() {
+    return (
+      <div>
+        Recipe Details
+      </div>
+    )
+  }
